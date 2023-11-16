46163e87f37e7a75efef43f9f09bcfab29e69d44
MUMER123 <uumer2428@gmail.com>
2023-11-16 14:49:08 +0500

    thread printing factorial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = sc.nextInt();
+
+        MyThread t = new MyThread("factorial",number);
+        t.start();
+        sc.close();
     }
 }
--- src/MyThread.java
+++ src/MyThread.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class MyThread {
+public class MyThread extends Thread{
+    private int num;
+    MyThread(String name,int num){
+        super(name);
+        this.num = num;
+    }
+    @Override
+    public void run(){
+        int fact = num;
+        if(num==0){
+            System.out.println(1);
+        }else {
+            for(int i=num-1;i>0;i--){
+                fact = fact*i;
+            }
+
+            System.out.println(fact);
+        }
+
+    }
+
 }
